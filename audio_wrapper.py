74697068c5d7395d433c5948157dab2b24b18925
PacoPacorius <c.perdikis@yahoo.gr>
2024-12-25 20:40:30 +0200

    audio_wrapper_read_data() ready

--- audio_wrapper.py
+++ audio_wrapper.py
@@ -1,15 +1,38 @@
+import sys
 import wave
+import numpy
 
-"""
-Is it read or write?
 
-First make it a Wave_read object
-"""
-#audio_data = wave.open("ena_dio_tria.wav", 'rb')
-audio_data = wave.open("/home/pacopacorius/test2.wav", 'rb')
-print(audio_data.getframerate(), '\n')
-bytes_per_sample = audio_data.getsampwidth()
-bytes_one_frame = audio_data.readframes(160) # this succesfully reads 160 frames from the audio_data
-print(audio_data.getsampwidth(), '\n')
-print(len(bytes_one_frame)) # the argument to hex() requires python 3.8 
-audio_data.close();
+def audio_wrapper_read_data(file_wav_audio, no_of_samples):
+    # open wav file in read format
+    audio_data = wave.open(file_wav_audio, 'rb')
+
+    # read 160 frames (samples) from the audio file
+    samples = audio_data.readframes(no_of_samples)
+
+    # does our sound file have 8-bits or 16-bits per sample?
+    sample_width = audio_data.getsampwidth()
+    audio_data.close()
+
+    # return one-dimensional numpy array with element size dependent on 
+    # sample width (in bytes)
+    if sample_width == 2:
+        return numpy.frombuffer(samples, numpy.int16)
+    elif sample_width == 1:
+        return numpy.frombuffer(samples, numpy.int8)
+    else:
+        sys.exit("""
+                Non-standard bits-per-sample WAV file. 
+                Only 8 or 16-bits are allowed, quitting...""")
+
+
+
+# get s0, then make it one-dimensional
+s0 = numpy.array([audio_wrapper_read_data("/home/pacopacorius/test2.wav", 160)])
+s0 = numpy.ravel(s0)
+print("Dimensions of numpy array s0 = ", numpy.ndim(s0), 
+      "\nSize of numpy array s0 = ", numpy.size(s0),
+      "\nShape of numpy array s0 = ", numpy.shape(s0),
+      "\nLength of numpy array s0 = ", len(s0),
+      "\nNumpy array s0 = ", s0,
+      "\ns0[5] = ", s0[5])
