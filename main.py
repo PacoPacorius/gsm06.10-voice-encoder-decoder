0304f57b39c613897aa8d7e1e264ff6d16a8fe87
PacoPacorius <c.perdikis@yahoo.g>
2025-02-16 21:39:17 +0200

    preprocessing update, long term code is ready for submission

--- main.py
+++ main.py
@@ -19,9 +19,6 @@ sample_rate,audio_data_o = audio_wrapper.scipy_read_data("ena_dio_tria.wav")
 iterations = len(audio_data_o) // 160     
 
 
-# pre-processing
-audio_data_of = preprocessing.offset_compensation(audio_data_o)
-audio_data = preprocessing.pre_emphasis(audio_data_of)
 
 #iterations = 1
 for j in range(0,iterations):
@@ -29,7 +26,8 @@ for j in range(0,iterations):
     s_new = numpy.zeros(160)
     offset = j * 160
     for i in range (offset, offset + 160):
-        s_new[i - offset] = audio_data[i]
+        s_new[i - offset] = audio_data_o[i]
+    # pre-processing
     s_of = preprocessing.offset_compensation(s_new)
     s    = preprocessing.pre_emphasis(s_of)
 
@@ -53,4 +51,4 @@ output_filename = 'reconstructed_audio.wav'
 scipy.io.wavfile.write(output_filename, sample_rate, audio_array)
 
 print('audio array = ', audio_array)
-print('audio data/starter = ', audio_data)
+print('audio data/starter = ', audio_data_o)
